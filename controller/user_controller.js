4e31ecc277972a8a2f386746cfd506e13006384e
Harsh <soni.hs661@gmail.comm>
2021-11-09 19:03:08 +0530

    add user controller and user route

---
+++ controller/user_controller.js
@@ -0,0 +1,4 @@
+module.exports.profile=function(req,res)
+{
+    return res.send('<h1>Profile </h1>')
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const router = express.Router();
 const homeController=require('../controller/home_controller');
+
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
 console.log("router loaded");
 module.exports=router;
