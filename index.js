a1fd3ffbc46219fb1e31ca93bf6bba5496121c87
Harsh <soni.hs661@gmail.comm>
2021-11-09 20:29:44 +0530

    setup view engine

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app=express();
 const port=8000;
 
 app.use('/',require('./routes'))
+//setup our view-engine
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err)
     {
