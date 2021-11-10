5aa284e2825241cc444dd725b795371fc32c34c4
Harsh <soni.hs661@gmail.comm>
2021-11-10 23:25:15 +0530

    edited the signin and signup routes

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -6,3 +6,12 @@ module.exports.signup=function(req,res)
 {
     return res.render('sign_up');
 }
+
+module.exports.createuser=function(req,res)
+{
+  //todolater
+}
+module.exports.createsession=function(req,res)
+{
+    //todolater
+}
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const router=express.Router();
+const userController=require('../controller/user_controller')
 
-
+router.get('/signin',userController.signin);
+router.get('/signup',userController.signup);
 module.exports=router;
