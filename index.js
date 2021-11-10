070e3320f48d22285377c07e64d7a53e9bad2355
Harsh <soni.hs661@gmail.comm>
2021-11-10 18:27:08 +0530

    setup assets folder

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express=require('express');
 const app=express();
-
+const expressLayouts=require('express-ejs-layouts');
 const port=8000;
 
-app.use('/',require('./routes'))
+app.use('/',require('./routes'));
+
 //setup our view-engine
 app.set('view engine','ejs');
 app.set('views','./views');
+app.use(expressLayouts);
+app.use(express.static('./assets'));
 app.listen(port,function(err){
     if(err)
     {
