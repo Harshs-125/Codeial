a0ffe6b74262a7803872aa9bc8e537818d2008f7
Harsh <soni.hs661@gmail.comm>
2021-11-22 18:36:17 +0530

    populate user to each post

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,5 +1,21 @@
+const Post=require('../models/post');
 module.exports.home=function(req,res){
+  // Post.find({},function(err,posts){
+  //   if(err)
+  //   {
+  //     console.log("error in finding post",err);
+  //   }
+  //   return res.render('home',{
+  //     title:"Home",
+  //     posts:posts
+  //   })
+  // })
+  
+  //Populating user of each post
+  Post.find({}).populate('user').exec(function(err,posts){
     return res.render('home',{
-      title:"Home"
+      title:"Home",
+      posts:posts
     })
+  })
 }
