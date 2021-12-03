a83b0bf3fa06c54f7d7e2896ca7972f5526f0e73
Harsh <soni.hs661@gmail.comm>
2021-12-03 17:57:48 +0530

    static methods in user model

--- models/users.js
+++ models/users.js
@@ -26,10 +26,13 @@ const storage = multer.diskStorage({
       cb(null,path.join(__dirname,'..', AVATAR_PATH));
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
       cb(null, file.fieldname + '-' + uniqueSuffix)
     }
-  })
+  });
+  //static methods
+  userSchema.statics.uploadedAvatar=multer({storage: storage}).single('avatar');
+  userSchema.statics.avatarPath=Avatar_PATH;
   
 
 const User=mongoose.model('User',userSchema);
