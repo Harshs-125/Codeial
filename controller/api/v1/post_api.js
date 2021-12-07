b1b73407b1bb6d30fcd99ece253d8acc42c3297d
Harsh <soni.hs661@gmail.comm>
2021-12-07 15:37:47 +0530

    show all post and delete a post using api

--- controller/api/v1/post_api.js
+++ controller/api/v1/post_api.js
@@ -1,6 +1,42 @@
-module.exports.post=function(req,res){
+const Post =require('../../../models/post');
+const Comment=require('../../../models/comments');
+module.exports.post=async function(req,res){
+    let posts= await Post.find({})
+  .sort('-createdAt')
+  .populate('user')
+  .populate({
+    path:'comments',
+    populate:{
+      path:'user'
+    }
+  })
     return res.json(200,{
         message:"Lists of Post",
-        post:[]
+        post:posts
     })
+}
+module.exports.deletePost=async function(req,res){
+    try{let post=await Post.findById(req.params.id);
+        //.id is given by mongoose converting object id into string
+        //if(post.user == req.user.id){
+           post.remove();
+           await Comment.deleteMany({post:req.params.id})
+        //    if(req.xhr)
+        //    {   
+        //        return res.status(200).json({
+        //            data:{
+        //                post_id:req.params.id
+        //            },
+        //            message:"post deleted"   
+        //        })
+        //    }
+        return res.json(200,{
+            message:"Deleted Post"
+        })
+    }catch(err)
+    {
+        return res.json(500,{
+            message:"internal servererror"
+        })
+    }
 }
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -2,5 +2,6 @@ const express=require('express');
 const router = express.Router();
 const postApi=require('../../../controller/api/v1/post_api');
 router.get('/',postApi.post);
+router.delete('/deletepost/:id',postApi.deletePost);
 
 module.exports=router;
