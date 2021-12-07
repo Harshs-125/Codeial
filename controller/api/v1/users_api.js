e4d957ffea401f718030765730a0382658ffd913
Harsh <soni.hs661@gmail.comm>
2021-12-07 17:38:13 +0530

    creating token by signing in user

---
+++ controller/api/v1/users_api.js
@@ -0,0 +1,26 @@
+const User=require('../../../models/users');
+const jwt=require('jsonwebtoken');
+
+module.exports.createSession=async function(req,res){
+    try{
+        let user=await User.findOne({email:req.body.email});
+        if(!user||user.password!=req.body.password)
+        {
+            return res.json(422,{
+                message:"Invalid Username/Password"
+            })
+        }
+        return res.json(200,{
+            message:"Sign in successfully here is your token please keep it safe",
+            data:{
+                token:jwt.sign(user.toJSON(),'codeial',{expiresIn:'360000'})
+            }
+        })
+    }catch(err)
+    {
+        console.log("error",err);
+        return res.json(500,{
+            message:"Internal server error"
+        });
+    }
+}
