c7ed01688d5f6c39dc956c6054ac96f7d5476314
Harsh <soni.hs661@gmail.comm>
2022-01-15 23:05:19 +0530

    gulp clean and build

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ const sass = require('gulp-sass')(require('sass'));
 const cssnano=require('gulp-cssnano');
 const rev=require('gulp-rev');
 const uglify=require('gulp-uglify-es').default;
+//const imagemin=require('gulp-imagemin');
+const del=require('del');
+
 
 gulp.task('css',function(done){
     console.log("minifying css");
@@ -31,4 +34,29 @@ gulp.task('js',function(done){
      }))
      .pipe(gulp.dest('./public/assets/js'));
      done();
+});
+
+gulp.task('image',function(done){
+    console.log('compressing images');
+    gulp.src('./uploads/**/*.+(png|jpg|gif|svg|jpeg)')
+     .pipe(imagemin())
+     .pipe(rev())
+     .pipe(gulp.dest('./public/assets/image'))
+     .pipe(rev.manifest({
+         cwd:'public',
+         merge:true
+     }))
+     .pipe(gulp.dest('./public/assets/image'));
+     done();
+
+});
+
+gulp.task('clean:assets',function(done){
+    del.sync('./public/assets');
+    done()
+});
+
+gulp.task('build',gulp.series('clean:assets','css','js'),function(done){
+    console.log("building assets");
+    done();
 })
