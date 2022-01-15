de953852952ee2abe6874836391c2c3ee2ace138
Harsh <soni.hs661@gmail.comm>
2022-01-15 18:56:42 +0530

    minify the css and js

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,33 @@ const gulp=require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cssnano=require('gulp-cssnano');
 const rev=require('gulp-rev');
+const uglify=require('gulp-uglify-es').default;
 
-gulp.task('css',function(){
+gulp.task('css',function(done){
     console.log("minifying css");
     gulp.src('./assets/scss/**/*.scss')
      .pipe(sass())
      .pipe(cssnano())
-     .pipe(gulp.dest('./assets.css'));
-
-    return gulp.src('./assets/**/*.css')
      .pipe(rev())
-     .pipe(gulp.dest('./public/assets/'))
+     .pipe(gulp.dest('./public/assets/css'))
      .pipe(rev.manifest({
          cwd:'public',
          merge:true
      }))
-     .pipe(gulp.dest('./public/assets'));
+     .pipe(gulp.dest('./public/assets/css'));
+     done();
+});
 
+gulp.task('js',function(done){
+    console.log('minifying js');
+    gulp.src('./assets/**/*.js')
+     .pipe(uglify())
+     .pipe(rev())
+     .pipe(gulp.dest('./public/assets/js'))
+     .pipe(rev.manifest({
+         cwd:'public',
+         merge:true
+     }))
+     .pipe(gulp.dest('./public/assets/js'));
+     done();
 })
