2c4c5f314d233539a64f368426bf12719d5bd487
Harsh <soni.hs661@gmail.comm>
2022-01-24 17:42:06 +0530

    db change

--- config/view-helper.js
+++ config/view-helper.js
@@ -1,15 +1,13 @@
 const env = require("./environment");
 const fs = require("fs");
 const path = require("path");
-
 module.exports = (app) => {
   app.locals.assetPath = function (filePath) {
-    if (env.name == "development") {
+    if (env.name == "developement") {
       return filePath;
     }
-
     return (
-      "/" +
+      "e/" +
       JSON.parse(
         fs.readFileSync(
           path.join(__dirname, "../public/assets/rev-manifest.json")
