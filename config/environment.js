148652cbc1f9cff01cbf45bc2e1ec52c4eb453a9
Harsh Soni <soni.hs661@gmail.com>
2022-02-21 00:16:49 +0530

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -9,31 +9,7 @@ const accessLogStream=rfs.createStream('access.log',{
 });
 
 
-const development={
-    name:'development',
-    asset_path:'./assets',
-    session_cookie_key:'somethingrandom',
-    db:'codiel_development',
-    smtp:{
-        service:'gmail',
-    host:'smtp.gmail.com',
-    port:587,
-    secure:false,
-    auth:{
-        user:'harshdvlpr@gmail.com',
-        pass:'mysispalak@123'
-    }
-    },
-    google_clientID:"1058656455912-vss5oj4iibg0oq7oqedv3ijl7ricjder.apps.googleusercontent.com",
-    goole_clientSecret:"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",
-    google_callbackURL:"http://localhost:8000/users/auth/google/callback",
-    jwt_secret:"codeial",
-    morgan:{
-        mode:'dev',
-        options:{stream:accessLogStream}
-    }
-     
-}
+
 const production={
     name:'production',
     asset_path:process.env.ASSETS_PATH,
